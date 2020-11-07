4a989a2e8067fd3026b7540c03a32f077e9f6ce1
WD <wojciech.dziadon@gmail.com>
2020-11-07 12:05:40 +0100

    dopisana  finkcaj dodająca ekwipunek

--- game_inv.py
+++ game_inv.py
@@ -1,14 +1,20 @@
 def display_inventory(inventory):
-
     for key, value in inventory.items():
         print(key+":",value)
 
+def add_to_inventory(inventory, added_items):
+    for elements in added_items:
+        for key in inventory:
+            if key == elements:
+                inventory[key] += 1
+    for elements in added_items:
+        if elements not in inventory:
+            inventory[elements] = 1
 
 
+              
 
-
-
-
-
-inventory = {"gold" : 25, "armor" : "silver", "helmet": "wood", "sword" : "strengthened"}
+inventory = {"gold" : 25, "armor" : 9, "helmet": 4, "sword" : 5}
+added_items = ('gold',"helmet","shoes")
+add_to_inventory(inventory,added_items)
 display_inventory(inventory)
